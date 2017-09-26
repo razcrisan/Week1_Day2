9d6ce9b4c181c8e1ad95f4916e40076247f9b0a3
razcrisan <rcrisan13@gmail.com>
2017-09-26 15:14:38 -0400

    Definitions for operators

--- Week1_Day2/Program.cs
+++ Week1_Day2/Program.cs
@@ -29,6 +29,42 @@ namespace Week1_Day2
             Console.WriteLine(partialNum);
             Console.WriteLine(someNumber);
 
+            //Now, let's talk about operators
+            // + adds two operands
+            // - Subtrcts the second operand from the first
+            // * Multiplies both operands
+            // / Divides the first operand by the other
+            // % Modulus - divides and checks remainder
+            // ++ Increments operator by one and is used for ints
+            // -- Decrements - decreases value by one
+            
+            //RELATIONAL OPERATORA
+            // == Checks if the values of two operands are equal, and if they are, returns bool true
+            // != Checks if values of two operands are equal or not, and if the values are NOT equal, returns bool true.
+            // not,
+            // if not, returns bool to true
+
+            int razAge = 23;
+
+            int monicaAge = 20;
+            Console.WriteLine(razAge == monicaAge);
+
+            /*
+             *  > Checks if the value of the left operand is
+             *  greater than the value of the right operand, and
+             *  if yes then condition is ture
+             *  < Checks the value of the left operand is less
+             *  than the value of the right operand, and 
+             *  if yes then condition is true
+             *  
+             *  >= Checks if the value of left operand is greater than
+             *  or equal to the value of the right operand,
+             *  and if yes than the condition becomes true.
+             *  
+             *  <= Checks if the value of the right operand is
+             *  less than or equal to the value of the right operand
+             *  and if yes then condition becomes true
+             *  */
 
 
         }
